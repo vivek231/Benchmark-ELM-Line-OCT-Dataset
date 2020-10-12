16bc356710f900ddb0763b04c6610118b45af199
vivek231 <vivekkr.singh90@gmail.com>
2020-10-12 15:05:37 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,147 +1,231 @@
-import os
-import torch
-from torch.utils.data import DataLoader, random_split
-import numpy as np
-from itertools import chain
-from skimage.color import rgb2gray
-from torch.utils import data
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import csv
-from torch import optim, sigmoid 
-from tqdm import tqdm
-from model import LinkNetImprove, LinkNet
-from torch import optim
-from torch.autograd import Variable
-from torchvision import datasets
-from torchvision import transforms
-import torchvision
-import warnings
-warnings.filterwarnings("ignore")
-from skimage import filters
-import skimage.io as io
+'''
+This code is written by:
+
+Dr. Vivek Kumar Singh
+Department of Computer Science
+Durham University, United Kingdom
+Date: 24/August/2020
+
+'''
 import argparse
-from skimage.color import rgb2gray
-from dataset import Dataset, RandomCrop, RandomFlip, ToTensor, Normalize
-import pydensecrf.densecrf as dcrf
-from pydensecrf.utils import unary_from_softmax, create_pairwise_bilateral, create_pairwise_gaussian
+import logging
+import os
+import sys
+import copy
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+from tqdm import tqdm
+from torchvision import transforms
+from eval import eval_net
+from model import U_Net,AttU_Net,LinkNetImprove,U2NETP,R2U_Net,DeepLabv3_plus,FCN,SegNet
+from all_transformers import get_all_transformers
+from tensorboardX import SummaryWriter
+from dataset import BasicDataset
+from torch.utils.data import DataLoader, random_split
+from dice_loss import Dice_Loss
+import torch.nn.functional as F
+from efficientunet import *
+import matplotlib.pyplot as plt
+import csv
+
+#------------- Load the images from directory----------
+train_dir_img = '/home/vivek/Documents/ELMseg/cross-validation/fold5/train/image/'
+train_dir_mask = '/home/vivek/Documents/ELMseg/cross-validation/fold5/train/mask/'
+val_dir_img = '/home/vivek/Documents/ELMseg/cross-validation/fold5/test/image/'
+val_dir_mask = '/home/vivek/Documents/ELMseg/cross-validation/fold5/test/mask/'
+dir_checkpoint = '/home/vivek/Documents/ELMseg/checkpoint/'
+# ------------------Loading END ------------------------
 
 
-parser = argparse.ArgumentParser(description='ELM_line segmentation-PyTorch-implementation')
-parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
-parser.add_argument('--num_epochs', type=int, default=200, help='number of epochs to train for')
-parser.add_argument('--n_classes', type=int, default=1, help='number of classes')
-parser.add_argument('--lr', type=float, default=0.0002, help='Learning Rate. Default=0.002')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
-parser.add_argument('--lamb', type=int, default=100, help='weight on L1 term in objective')
-opt = parser.parse_args()
+def dice_loss(pred, target, smooth = 1.):
+    pred = pred.contiguous()
+    target = target.contiguous()
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+    return loss.mean()
 
-# --------------- Check GPU is available or not -------------
-cuda = torch.cuda.is_available()
-device = torch.device('cuda:0' if cuda else 'cpu')
+n_classes =1
+n_channels = 3
 
-# -------------------- Model load ------------------------
-n_classes = opt.n_classes
-model = LinkNet(classes = n_classes)
-pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print ("\nTrainable parameters = ", pytorch_total_params)
+def train_net(net,
+              device,
+              epochs=15,
+              batch_size=4,
+              lr=0.0001,
+              val_percent=0.1,
+              save_cp=True,
+              img_scale=1):
+    
+    transform = get_all_transformers()
+    train_dataset = BasicDataset(train_dir_img, train_dir_mask, img_scale,transform = transform['train'])
+    
+    val_dataset= BasicDataset(val_dir_img, val_dir_mask, img_scale,transform['val'])
+    n_train=len(train_dataset)
+    n_val=len(val_dataset)
 
-# -------------------- Dataloader path -----------------
-OCT_train = '/home/vivek/Music/Eye_net/dataset/train/' 
-OCT_val = '/home/vivek/Music/Eye_net/dataset/val/'
-OCT_test = '/home/vivek/Music/Eye_net/dataset/test/'
-result = '/home/vivek/Music/Eye_net/result/'
+# ----------- Load the dataset from the directory---------
 
-batch_size = opt.batch_size
-print('===> Loading datasets')
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True, drop_last=True)
 
-# --------------------- Loading train and validation data from directory --------------------
-transform_train = transforms.Compose([RandomFlip(), ToTensor()])
-transform_val = transforms.Compose([ToTensor()])
-transform_test = transforms.Compose([ToTensor()])
+    writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
+    global_step = 0
 
-train_dataset = Dataset(OCT_train, data_type='float32', transform = transform_train)
-val_dataset = Dataset(OCT_val, data_type='float32', transform = transform_val)
-test_dataset = Dataset(OCT_test, data_type='float32',  transform = transform_test)
-train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle=True) 
-val_loader = DataLoader(val_dataset, batch_size = batch_size, shuffle=False) 
-test_loader = DataLoader(test_dataset, batch_size = batch_size, shuffle=False) 
-num_train = len(train_loader)
-num_batch_train = int((num_train / batch_size) + ((num_train % batch_size) != 0))
+    logging.info(f'''Starting training:
+        Epochs:          {epochs}
+        Batch size:      {batch_size}
+        Learning rate:   {lr}
+        Training size:   {n_train}
+        Validation size: {n_val}
+        Checkpoints:     {save_cp}
+        Device:          {device.type}
+        Images scaling:  {img_scale}
+    ''')
 
-loss_index = []
-loss_values = []
-List_Loss = []
+# !!---------- Defined the optimizer --------------------------!!
 
-# ----------- Loss function ----
-criterion_l1 = nn.L1Loss()
-criterion = nn.BCEWithLogitsLoss()
-params = list(model.parameters())
+    optimizer = optim.Adam(net.parameters(), lr = lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-9)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min' if net.n_classes > 1 else 'max', patience=10)
 
-# -------- Hyperparameters -------
-num_epochs = opt.num_epochs
-learning_rate = opt.lr 
-st_epoch = 0
-num_epoch = 100
-# ------------ Optimizer -----------
-optimizer = torch.optim.Adam(model.parameters(), learning_rate, betas=(opt.beta1, 0.999))
-# --------- Training loop --------
-for epoch in range(st_epoch + 1, num_epoch + 1):
-    model.train()
-    epoch_loss = []
-    for i, data in enumerate(train_loader, 1):
-        def should(freq):
-            return freq > 0 and (i % freq == 0 or i == num_batch_train)
-        input = data['input'].to(device)
-        label = data['label'].to(device)
-        input = Variable(input.float(), requires_grad=False)
-        label = Variable(label.float(), requires_grad=False)
-# ----- Forward pass--------
-        out = model(input)
-        out = out.squeeze(1)
-        label = label.squeeze(1)
-        loss = criterion(out,label)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        epoch_loss += [loss.item()]
+# ------------------ Loss function ------------------!!
+    criterion_dice = Dice_Loss
+    criterion = nn.BCEWithLogitsLoss().cuda()
+    
+# !!-------------- Training and validation loop ------------------!!
+    loss_index = []
+    loss_values = []
+    List_Loss = []
+    best_acc=0
+    for epoch in range(epochs):
+        net.train()
 
-        print('TRAIN: EPOCH %d: BATCH %04d/%04d: LOSS: %.4f'
-                % (epoch, i, num_batch_train, np.mean(epoch_loss)))
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                imgs = batch['image']
+                true_masks = batch['mask']
+                imgs = imgs.to(device=device, dtype=torch.float32)
+                mask_type = torch.float32 if net.n_classes == 1 else torch.long
+                true_masks = true_masks.to(device=device, dtype=mask_type)
 
-        loss_index.append(epoch)
-        loss_values.append(epoch_loss) 
-        List_Loss.append([epoch, epoch_loss])
+                masks_pred = net(imgs)
+                out_new =  F.sigmoid(masks_pred)
 
-        with open('Loss/Training_Loss.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(List_Loss)
-        if  epoch % 1 == 0:
-            torch.save(model.state_dict(),'model/Eyenet{:d}.ckpt'.format(epoch))
-            dirName = result + str(epoch)
-            if not os.path.exists(dirName):
-                os.mkdir(dirName)
-            visualization = dirName + "/"
+                # masks_probs_flat = masks_pred.view(-1)
+                # true_masks_flat = true_masks.view(-1)
+                loss = criterion(masks_pred, true_masks)+criterion_dice(out_new, true_masks)
+               
+                
+                epoch_loss += loss.item()
+                writer.add_scalar('Loss/', loss.item(), global_step)
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
 
-        with torch.no_grad():
-            model.eval()
-            for j, data in enumerate(test_loader, 1):
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_value_(net.parameters(), 0.1)
+                optimizer.step()
 
-                input = data['input'].to(device)
-                label = data['label'].to(device)
+                loss_index.append(epoch) 
+                loss_values.append(epoch_loss)   
+                List_Loss.append([epoch, epoch_loss])
 
-                input = Variable(input.float(), requires_grad=False)
-                label = Variable(label.float(), requires_grad=False)
-                output = model(input)
-                output = torch.sigmoid(output)
-                output = 1.0 * (output > 0.5)
-#  -------------- save all the visualiztion ---------------- 
-                torchvision.utils.save_image(input, visualization+'/'+ str(j)+'_input.png')
-                torchvision.utils.save_image(output, visualization+'/'+ str(j)+'_pred.png')
-                torchvision.utils.save_image(label, visualization+'/'+ str(j)+'_label.png')
+                pbar.update(imgs.shape[0])
+                global_step += 1
+                if global_step % ((n_val+n_train) // (10 * batch_size)) == 0:
+                    for tag, value in net.named_parameters():
+                        tag = tag.replace('.', '/')
+                        writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
+                        #writer.add_histogram('grads/' + tag, value.grad.data.cpu().numpy(), global_step)
+                    val_score = eval_net(net, val_loader, device)
+                    scheduler.step(val_score)
+                    writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], global_step)
 
-        plt.plot(loss_index,loss_values)
-        plt.ylabel('train loss')
-        plt.xlabel('Epochs')
-        plt.savefig('train_loss/train_loss'+str(epoch)+'.png')
+                    if net.n_classes > 1:
+                        logging.info('Validation cross entropy: {}'.format(val_score))
+                        writer.add_scalar('Loss/test', val_score, global_step)
+                    else:
+                        logging.info('Validation Dice Coeff: {}'.format(val_score))
+                        writer.add_scalar('Dice/test', val_score, global_step)
+                    
+                    if val_score > best_acc:
+                        best_acc = val_score
+                        best_model_wts = copy.deepcopy(net.state_dict())
+
+                    writer.add_images('images', imgs, global_step)
+                    if net.n_classes == 1:
+                        writer.add_images('masks/true', true_masks, global_step)
+                        writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
+
+    if save_cp:
+        try:
+            os.mkdir(dir_checkpoint)
+            logging.info('Created checkpoint directory')
+        except OSError:
+            pass
+        net.load_state_dict(best_model_wts)
+        t = time.localtime()
+        timestamp = time.strftime('%b-%d-%Y_%H%M', t)
+        torch.save(net.state_dict(), '{}ELM_{}.model'.format(dir_checkpoint,timestamp))
+
+    writer.close()
+
+def get_args():
+    parser = argparse.ArgumentParser(description='2D ELM layer segmentation from OCT images',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=40,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=6,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.0001,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--scale', dest='scale', type=float, default=1,
+                        help='Downscaling factor of the images')
+    parser.add_argument('-v', '--validation', dest='val', type=float, default=15.0,
+                        help='Percent of the data that is used as validation (0-100)')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    args = get_args()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')   
+
+# -------------- Load the model -----------
+    #net = get_efficientunet_b3(n_classes=1, concat_input=True, pretrained=True)
+    #net = LinkNetImprove(n_channels=3, n_classes=1)
+    #net = AttU_Net(n_channels=3, n_classes=1)
+    #net = U_Net(n_channels=3, n_classes=1)
+    #net = R2U_Net(n_channels=3, n_classes=1,t=2)
+    #net = DeepLabv3_plus(n_channels=3, n_classes=1, os=16, pretrained=True, _print=True)
+    #net = FCN(n_channels=3, n_classes=1)
+    net = SegNet(n_channels=3, n_classes=1)
+
+    if args.load:
+        net.load_state_dict(
+            torch.load(args.load, map_location=device)
+        )
+        logging.info(f'Model loaded from {args.load}')
+    net.to(device=device)
+    try:
+        train_net(net=net,
+                  epochs=args.epochs,
+                  batch_size=args.batchsize,
+                  lr=args.lr,
+                  device=device,
+                  img_scale=args.scale,
+                  val_percent=args.val / 100)
+    except KeyboardInterrupt:
+        torch.save(net.state_dict(), 'INTERRUPTED.pth')
+        logging.info('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
